5ff49d7b3f1c5fabf06fb245d55e98b150a95ba1
Marcus Hanwell <marcus.hanwell@kitware.com>
2010-04-02 18:16:53 +0000

    BUG: Title can be null...

--- Charts/vtkAxis.cxx
+++ Charts/vtkAxis.cxx
@@ -418,7 +418,10 @@ float vtkAxis::NiceNumber(float n, bool roundUp)
 void vtkAxis::PrintSelf(ostream &os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
-  os << indent << "Axis title: \"" << *this->Title << "\"" << endl;
+  if (this->Title)
+    {
+    os << indent << "Axis title: \"" << *this->Title << "\"" << endl;
+    }
   os << indent << "Minimum point: " << this->Point1[0] << ", "
      << this->Point1[1] << endl;
   os << indent << "Maximum point: " << this->Point2[0] << ", "
