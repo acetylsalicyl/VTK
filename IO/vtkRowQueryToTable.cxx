17e8b88d59e7390e6b2031a7a460de420967cc41
Jeffrey Baumes <jeff.baumes@kitware.com>
2010-04-12 18:52:49 +0000

    BUG: Should default to double when there is no data to process.

--- IO/vtkRowQueryToTable.cxx
+++ IO/vtkRowQueryToTable.cxx
@@ -106,6 +106,10 @@ int vtkRowQueryToTable::RequestData(
       {
       arr = vtkTypeUInt64Array::New();
       }
+    else if (type == 0)
+      {
+      arr = vtkAbstractArray::CreateArray(VTK_DOUBLE);
+      }
     else
       {
       arr = vtkAbstractArray::CreateArray(type);
